a0a286fde6ad3877427633220cb3aa6c1952d211
Kevin <tlk999@live.co.uk>
2020-05-26 20:50:31 +0100

    0.02 Strings1

---
+++ strings1.rb
@@ -0,0 +1,12 @@
+x = "hello "
+y = "world"
+puts x + y
+puts (x + y).upcase()
+puts "  Hello World  ".strip()
+puts "  Hello World  ".length()
+puts "  Hello World  ".include? "World"
+puts "  Hello World  "[4]
+
+x = 1
+y = 2
+puts #{ x + y }
