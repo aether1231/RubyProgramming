16d94afea3049935d828fe1edeab53c3d7b2d87c
Kevin <tlk999@live.co.uk>
2020-05-27 11:26:21 +0100

    0.03 Strings1

--- strings1.rb
+++ strings1.rb
@@ -1,12 +1,17 @@
-x = "hello "
-y = "world"
-puts x + y
-puts (x + y).upcase()
-puts "  Hello World  ".strip()
-puts "  Hello World  ".length()
-puts "  Hello World  ".include? "World"
-puts "  Hello World  "[4]
-
-x = 1
-y = 2
-puts #{ x + y }
+x = "Hello "
+y = "World"
+puts "x + y string concatination: " + (x + y)
+puts "upcase string method: " + (x + y).upcase
+puts "downcase string method: " + (x + y).downcase
+puts " Double-quoted strings support string interpolation #{ x }"
+puts ' Single-quoted strings don\'t support string interpolation #{ x }'
+puts "strip string method: " + "  Hello World  ".strip()
+puts "length string method: " + "Hello World".length.to_s
+puts "reverse string method: " + "dohteM esreveR".reverse
+puts "incude string method: " + ("  Hello World  ".include? "World").to_s
+puts "get 5th character of 'Hello World': " + "Hello World"[4]
+puts %{ Arbitrary \{\} delimiters. }
+puts %! Arbitrary \!\! delimiters. !
+puts %Q{ %Q\{\} equivalent to double-quoted string }
+puts %q[ %q\[\] equivalent to single-quoted string ]
+puts %x! ls; echo "%x\!\! executes command line " !
