5e0472017811d85a860708b41375285a14b7fa8c
Kevin <tlk999@live.co.uk>
2020-05-27 21:52:42 +0100

    3.01 Blocks and Sorting

--- methodsBlocksSorting.rb
+++ methodsBlocksSorting.rb
@@ -6,4 +6,41 @@ def factorial(i)
     return j
 end
 
+def add(*args)
+    sum = 0;
+    args.each{|arg|
+        sum += arg
+    }
+    return sum
+end
+
+def are_contiguous?(*args)
+    contig = false
+    args.each{|arg|
+        next if arg == args[0]
+        puts "#{arg}"
+        
+    }
+end
+
+puts 'function with fixed number of arguments'
 puts factorial(5).to_s
+
+puts 'function with variable number of arguments'
+puts add(1,2,3,4,5).to_s
+are_contiguous?(1,2,3,4,5).to_s
+
+puts 'simple sort'
+puts [2,4,7,10,13,1,-2,5,21,44].sort!
+
+puts 'combined comparison operator <=>'
+puts -1 <=> 1
+puts 1 <=> 1
+puts 1 <=> -1
+
+puts 'combined comparison operator <=> in block sort'
+books = ["Charlie and the Chocolate Factory", "War and Peace", "Utopia", "A Brief History of Time", "A Wrinkle in Time"]
+books.sort!{|firstbook, secondbook|
+    firstbook <=> secondbook
+}
+puts books
