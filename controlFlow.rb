0dd2891c1f7449d2bfecac73a37923168995d18a
Kevin <tlk999@live.co.uk>
2020-05-27 13:12:59 +0100

    1.01 Logical Operators

--- controlFlow.rb
+++ controlFlow.rb
@@ -1,9 +1,30 @@
 print 'Input an integer: '
 i = Integer(gets.chomp)
-if i < 0
-    puts 'Negative integer'
-elsif i > 0
-    puts 'Positive integer'
+puts 'Input another integer: '
+j = Integer(gets.chomp)
+# if i < 0
+#     puts 'Negative integer'
+# elsif i > 0
+#     puts 'Positive integer'
+# else
+#     puts 'Zero'
+# end
+
+unless i == j
+    puts "i and j are different"
 else
-    puts 'Zero'
+    puts "i and j are equal"
 end
+
+puts '! operator'
+puts '!true = ' + (!true).to_s
+puts "\n&& operator"
+puts 'true && true = ' + (true && true).to_s
+puts 'true && false = ' + (true && false).to_s
+puts 'false && true = ' + (false && true).to_s
+puts 'false && false = ' + (false && false).to_s
+puts "\n|| operator"
+puts 'true || true = ' + (true || true).to_s
+puts 'true || false = ' + (true || false).to_s
+puts 'false || true = ' + (false || true).to_s
+puts 'false || false = ' + (false || false).to_s
